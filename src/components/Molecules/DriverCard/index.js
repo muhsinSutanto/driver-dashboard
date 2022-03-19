403242c0779bf72b5f50d98885c07727330cf3b4
muhsin sutanto <sutantomuhsin@gmail.com>
2022-03-19 17:34:23 +0700

    update syling on DriverCard

--- src/components/Molecules/DriverCard/index.js
+++ src/components/Molecules/DriverCard/index.js
@@ -1,3 +1,5 @@
+import { IconButton } from "@material-ui/core";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import React from "react";
 import { useStyles } from "./style";
 
@@ -10,16 +12,23 @@ const DriverCard = (props) => {
             <p>
                Driver ID <span>{driver.id.value}</span>
             </p>
-            <p>...</p>
+
+            <IconButton color="inherit" aria-label="open drawer" edge="start">
+               <MoreHorizIcon />
+            </IconButton>
          </div>
          <div>
             <img src={driver.picture.thumbnail} />
-            <div>
-               <h1>Nama Driver</h1>
-               <p>
-                  {driver.name.first} {driver.name.second}
-               </p>
-            </div>
+            <h1>Nama Driver</h1>
+            <p>
+               {driver.name.first} {driver.name.last}
+            </p>
+            <h1>Telepon</h1>
+            <p>{driver.cell}</p>
+            <h1>Email</h1>
+            <p>{driver.email}</p>
+            <h1>Tanggal Lahir</h1>
+            <p>{driver.dob.date.substr(0, 10)}</p>
          </div>
       </div>
    );
--- src/components/Molecules/DriverCard/style.js
+++ src/components/Molecules/DriverCard/style.js
@@ -4,10 +4,42 @@ import { COLORS } from "../../../const/styles";
 const useStyles = makeStyles((theme) => ({
    cardContainer: {
       minWidth: "300px",
-      padding: "16px 20px",
       borderRadius: "8px",
       marginRight: "24px",
       background: COLORS.WHITE,
+      "& p": {
+         color: COLORS.GREY,
+         fontSize: "14px",
+         "& span": {
+            color: COLORS.RED_PRIMARY,
+         },
+      },
+      "& div:nth-child(1)": {
+         display: "flex",
+         justifyContent: "space-between",
+         borderBottom: `2px solid ${COLORS.GREY}`,
+         padding: "12px 16px 0px 16px",
+         alignItem: "center",
+      },
+
+      "& div:nth-child(2)": {
+         padding: "20px 16px 0px 16px",
+         "& img": {
+            borderRadius: "50%",
+            width: "80px",
+         },
+         "& h1": {
+            margin: "0px",
+            fontSize: "12px",
+            color: COLORS.GREY,
+         },
+         "& p": {
+            margin: "0px",
+            fontSize: "14px",
+            color: COLORS.BLACK,
+            paddingBottom: "12px",
+         },
+      },
    },
 }));
 
